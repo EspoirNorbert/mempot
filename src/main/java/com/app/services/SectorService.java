23464817d9d839856cb23b65b78d3c14245b36a0
Nores <70499515+EspoirNorbert@users.noreply.github.com>
2023-10-11 01:30:48 +0200

    CRUD Sector: Implementation GradeService

---
+++ src/main/java/com/app/services/SectorService.java
@@ -0,0 +1,44 @@
+package com.app.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.models.Sector;
+import com.app.repositories.SectorRepository;
+
+@Service
+public class SectorService {
+
+	@Autowired
+	private SectorRepository sectorRepository;
+	
+	public List<Sector> list() {
+		return sectorRepository.findAll();
+	}
+	
+	public void save(Sector grade) {
+		sectorRepository.save(grade);
+	}
+	
+	@Transactional(readOnly = true)
+	public Sector findById(Integer id) {
+		Sector grade = null;
+		Optional<Sector> optionalGrade = sectorRepository.findById(id);
+		
+		if (optionalGrade.isPresent())
+			grade = optionalGrade.get();
+		
+		return grade;
+	}
+	
+	@Transactional
+	public void delete (Integer id) {
+		Sector grade = this.findById(id);
+		if (grade != null)
+			this.sectorRepository.delete(grade);
+	}
+}
