91c9c64655113c72d8b83a8c936d91b3080d7280
Nores <70499515+EspoirNorbert@users.noreply.github.com>
2023-10-10 20:08:18 +0200

    Create grade and sector object

---
+++ src/main/java/com/app/models/Sector.java
@@ -0,0 +1,54 @@
+package com.app.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sectors")
+public class Sector implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 30)
+	private String name;
+	
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	
+	public Sector() {}
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
